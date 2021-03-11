b960a5e1c9c91fde1c08076b7fc3a3cb7aeec096
nestor-urchin <68887976+nestor-urchin@users.noreply.github.com>
2021-03-11 17:44:40 +0700

    Create Exercise5_1_Saksorn_P.py

---
+++ Exercise5_1_Saksorn_P.py
@@ -0,0 +1,6 @@
+x = int(input('Choose first number: '))
+y = int(input('Choose second number: '))
+print(x, '+', y, '=', x+y)
+print(x, '-', y, '=', x-y)
+print(x, '*', y, '=', x*y)
+print(x, '/', y, '=', x/y)
